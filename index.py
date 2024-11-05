77899880a932a5ffe82e3e17ce1d85f5dceda882
hibamallat <hibamallat040@gmail.com>
2024-11-05 20:21:13 +0200

    Take the list from the user

---
+++ index.py
@@ -0,0 +1,22 @@
+binary_list = []
+
+def longest_subarray(array):
+
+
+
+
+    return 
+
+for i in range (10):
+    num_binary = int(input("Enter a binary number: "))
+    if num_binary == 0 or num_binary == 1:
+        binary_list.append(num_binary)
+    elif num_binary == -1:
+        break
+    else:
+        print("The number should be binary")
+        
+
+print("", binary_list)
+
+print("", longest_subarray(binary_list))

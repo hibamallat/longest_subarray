292e029b45db567e31e8dd18eae040d48f21457f
hibamallat <hibamallat040@gmail.com>
2024-11-06 02:52:54 +0200

    The function longest subarray is work

--- index.py
+++ index.py
@@ -1,4 +1,24 @@
-binary_list = []
+binary_list = [] #[1,1,1,0]
+
+def longest_subarray(array):
+    max_length = 0
+
+    for i in range (len(array)):
+        count_zeros = 0
+        count_ones = 0
+
+        for j in range(i, len(array)):
+
+            if array[j] == 0:
+                count_zeros += 1
+            else:
+                count_ones += 1
+
+            if count_zeros == count_ones:
+                subarray_length = j - i + 1
+                max_length = max(max_length, subarray_length)
+
+    return max_length
 
 for i in range (10):
     num_binary = int(input("Enter a binary number: "))
@@ -9,5 +29,6 @@ for i in range (10):
     else:
         print("The number should be binary")
         
+print("The original list:", binary_list)
+print("Length = ", longest_subarray(binary_list)) #length = 2  
 
-print("", binary_list)

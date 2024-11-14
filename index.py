8d0fcac4aa0129ea364ae7caaae3356347fee1bc
hibamallat <hibamallat040@gmail.com>
2024-11-14 18:05:03 +0200

    Add few comment lines and change some variables

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-binary_numbers = []
 
 def longest_subarray(array):
     max_length = 0
@@ -6,7 +5,8 @@ def longest_subarray(array):
     for i in range (len(array)):
         count_zeros = 0
         count_ones = 0
-
+        
+        # for each start point, extend the subarray to the end
         for j in range(i, len(array)):
 
             if array[j] == 0:
@@ -14,25 +14,33 @@ def longest_subarray(array):
             else:
                 count_ones += 1
 
+            # If the subarray has equal 0s and 1s, check its length
             if count_zeros == count_ones:
-                balanced_length = j - i + 1
-                max_length = max(max_length, balanced_length)
+                length = j - i + 1
+
+                # Update max_length if the current subarray is the longest found
+                if length > max_length:
+                    max_length = length
 
     return max_length
+
+# Prepare to get binary numbers from the user
+binary_numbers = []
 try:
-    for i in range (10):
-        num_binary = int(input("Enter a binary number:(Enter -1 to stop) "))
+    list_size = int(input("Enter the number of binary values you want to add to the list: "))
+
+    for i in range (list_size):
+        num_binary = int(input("Enter a binary number: "))
         if num_binary == 0 or num_binary == 1:
             binary_numbers.append(num_binary)
-        elif num_binary == -1:
-            break
         else:
-            print("The number should be binary")
+            print("Enter binary numbers (0 or 1)")
 
-    print("The original list:", binary_numbers)
-    print("Length of longest balanced subarray: ", longest_subarray(binary_numbers)) 
+    print(f"List of numbers you entered: {binary_numbers}")
+    print(f"Length of the longest subarray with equal 0s and 1s: {longest_subarray(binary_numbers)}") 
 
 except ValueError:
-    print("It should be a number")
+    # If they enter something that’s not a number, show this message
+    print("Enter valid binary numbers (0 or 1) only")
 
 
